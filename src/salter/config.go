44dd9936ede0fa0219b004de57abae5084d688a5
Dave Smith <dizzyd@dizzyd.com>
2014-07-03 09:54:00 -0600

    Add environment value to UserDataVars

--- src/salter/config.go
+++ src/salter/config.go
@@ -206,6 +206,7 @@ type UserDataVars struct {
 	Roles []string
 	IsMaster bool
 	Grains map[string]string
+	Environment string
 }
 
 func (config *Config) generateUserData(host string, roles []string, masterIp string) ([]byte, error) {
@@ -217,6 +218,7 @@ func (config *Config) generateUserData(host string, roles []string, masterIp str
 			Roles: roles,
 			IsMaster: (masterIp == "127.0.0.1"),
 			Grains: config.Salt.Grains,
+			Environment: "test",
 		})
 	if err != nil {
 		fmt.Printf("Failed to generate user-data for %s: %+v\n", host, err)
